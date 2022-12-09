f90af3d28cb24dd76f3990c1ccbbe43198dde993
DVB <dvbaron@gmx.fr>
2022-12-09 14:33:20 +0100

    modif 2

--- node/routes/index.js
+++ node/routes/index.js
@@ -6,6 +6,6 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello Git!' });
-});
+}) ;
 
 module.exports = router;
